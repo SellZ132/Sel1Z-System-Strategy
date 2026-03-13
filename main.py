7a9c000d0c29b5df82370d7bb7f64f69b1800252
Sel1Z <166610193+SellZ132@users.noreply.github.com>
2026-03-14 05:00:55 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord import ui
 
 # คำเตือน: Token นี้เป็นข้อมูลสำคัญ ห้ามเผยแพร่สู่สาธารณะนะครับ
-TOKEN = 'TOKEN'
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
